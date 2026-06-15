49a654519315bcf6cd4d2c8e96299cf5874ae7a9
elizabethantonyt-ai <elizabethantonyt@gmail.com>
2026-06-15 18:42:28 +0100

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,9 @@
+x = 5
+y = "john]" 
+print(x)
+print(y)
+codingal = "codingal"
+print(codingal) 
+user = input("enter your name")
+print("hello",user,"welcome to codingal")
+
---
+++ welcom.py
@@ -0,0 +1,4 @@
+print("welcome to the world of coding")
+print(4)
+print("hello python 4\n")
+print("hello my name is alwin",5)
